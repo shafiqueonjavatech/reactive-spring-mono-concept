708fa2436f6b6bc71b914f37622ba0e0efe8435c
mohammed-shafique-maersk <mohammed.shafique@lns.maersk.com>
2025-05-03 13:25:13 +0200

    update -> added a trasformation service

---
+++ src/main/java/com/shafique/service/MonoTransformationService.java
@@ -0,0 +1,21 @@
+package com.shafique.service;
+
+import reactor.core.publisher.Mono;
+
+public class MonoTransformationService {
+    
+    //Mono  transformation Operator
+    public Mono<String> mapToUpper(){
+        return Mono.just ( "hello" ).map ( String::toUpperCase );
+    }
+    
+    
+    public Mono<Integer> convertToLength(){
+        return Mono.just ( "Banana" ).flatMap ( element -> Mono.just ( element.length () ) );
+    }
+    
+    public Mono<Integer> filterMono(){
+        return Mono.just ( 734 ).filter (  i -> i < 300 ).defaultIfEmpty ( Integer.MIN_VALUE );
+    }
+    
+}
