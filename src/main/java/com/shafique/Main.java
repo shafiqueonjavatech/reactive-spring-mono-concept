430bd7b67166d234e7476efc3f14210aab29034b
mohammed-shafique-maersk <mohammed.shafique@lns.maersk.com>
2025-05-03 02:55:31 +0200

    update -> test reactor is configured properly

--- src/main/java/com/shafique/Main.java
+++ src/main/java/com/shafique/Main.java
@@ -1,7 +1,10 @@
 package com.shafique;
 
+import com.shafique.service.MonoService;
+
 public class Main {
     public static void main ( String[] args ) {
-
+        MonoService monoService = new MonoService ();
+        monoService.legendMono ().doOnNext ( System.out::print ).subscribe ();
     }
 }
