ab7f025654ebe9eeab9b34ef88c5f4d6e29695bf
mohammed-shafique-maersk <mohammed.shafique@lns.maersk.com>
2025-05-03 12:14:22 +0200

    update -> added code to clarify usage of defer

--- src/main/java/com/shafique/Main.java
+++ src/main/java/com/shafique/Main.java
@@ -1,9 +1,10 @@
 package com.shafique;
 
 import com.shafique.service.MonoService;
+import reactor.core.publisher.Mono;
 
 public class Main {
-    public static void main ( String[] args ) {
+    public static void main ( String[] args ) throws InterruptedException {
         MonoService monoService = new MonoService ();
         
         // creating an empty mono and setting default text to verify
@@ -13,7 +14,25 @@ public class Main {
         // creating legend mono
         monoService.legendMono().doOnNext( System.out::println ).subscribe();
         
+        // test mono trigger based on operator
+        monoService.monoJustEager();
+        
         // creating mono from callable
         monoService.monoFromCallable().doOnNext( System.out::println ).subscribe();
+        
+        // creating mono from supplier
+        monoService.monoFromSupplier();
+        
+        // compare defer with just, intentionally added this to make it clear the usage
+        // of defer during code review, i saw few of you people used it incorrectly.
+        Mono<Long> mono1 = monoService.monoDeferCompareJust ();
+        mono1.doOnNext ( System.out::println ).subscribe ();
+        Thread.sleep(1000);
+        mono1.doOnNext ( System.out::println ).subscribe ();
+        
+        Mono<Long> mono2 = monoService.monoDefer ();
+        mono2.doOnNext ( System.out::println ).subscribe ();
+        Thread.sleep(1000);
+        mono2.doOnNext ( System.out::println ).subscribe ();
     }
 }
--- src/main/java/com/shafique/service/MonoService.java
+++ src/main/java/com/shafique/service/MonoService.java
@@ -18,5 +18,29 @@ public class MonoService {
         } );
     }
     
+    public Mono<String> monoFromSupplier(){
+        return Mono.fromSupplier ( ()-> returnSomeValue () );
+    }
+    
+    public Mono<String> monoJustEager(){
+        Mono<String> test =  Mono.just( returnSomeValue () );
+        return Mono.empty ();
+    }
+    
+    
+    private String returnSomeValue(){
+        return "i was executed eagerly";
+    }
+    
+    
+    public Mono<Long> monoDefer(){
+        return Mono.defer( () -> Mono.just ( System.currentTimeMillis () ));
+    }
+    
+    public Mono<Long> monoDeferCompareJust(){
+        return Mono.just ( System.currentTimeMillis () );
+    }
+    
+    
     
 }
