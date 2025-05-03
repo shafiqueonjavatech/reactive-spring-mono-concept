abba24abddc3b2038aa43382b595669c197cfad1
mohammed-shafique-maersk <mohammed.shafique@lns.maersk.com>
2025-05-03 03:12:46 +0200

    update -> mono creation operator

--- src/main/java/com/shafique/Main.java
+++ src/main/java/com/shafique/Main.java
@@ -5,6 +5,15 @@ import com.shafique.service.MonoService;
 public class Main {
     public static void main ( String[] args ) {
         MonoService monoService = new MonoService ();
-        monoService.legendMono ().doOnNext ( System.out::print ).subscribe ();
+        
+        // creating an empty mono and setting default text to verify
+        monoService.emptyMono().defaultIfEmpty ( "Nothing emitted" )
+                .doOnNext( element ->System.out.println ( "values is :"+element )).subscribe ();
+        
+        // creating legend mono
+        monoService.legendMono().doOnNext( System.out::println ).subscribe();
+        
+        // creating mono from callable
+        monoService.monoFromCallable().doOnNext( System.out::println ).subscribe();
     }
 }
--- src/main/java/com/shafique/service/MonoService.java
+++ src/main/java/com/shafique/service/MonoService.java
@@ -4,7 +4,19 @@ import reactor.core.publisher.Mono;
 
 public class MonoService {
     
-    public Mono<String> legendMono(){
-        return Mono.just ( "Mahatma Gandhi" );
+    public Mono<String> emptyMono(){
+        return Mono.empty();
     }
+    
+    public Mono<String> legendMono(){
+        return Mono.just( "Mahatma Gandhi" );
+    }
+    
+    public Mono<String> monoFromCallable(){
+        return Mono.fromCallable( ()-> {
+            return "i am returning inline";
+        } );
+    }
+    
+    
 }
